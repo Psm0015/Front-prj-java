4c1b4e94d8f0fda92747b0158f4aea15f9d00da7
pedro-mota <pedro.mota@df.estudante.senai.br>
2022-11-26 20:53:28 -0300

    Atualização api de produtos para local

--- js/scriptprodutos.js
+++ js/scriptprodutos.js
@@ -2,7 +2,7 @@ ajax = new XMLHttpRequest();
 var prList;
 listar()
 function listar(){
-    ajax.open("GET","https://pedro-mota-qua209050.herokuapp.com/api/produto/");
+    ajax.open("GET","http://localhost:8080/api/produto/");
     ajax.send();
     ajax.onload = function(){
         prList=this.responseText;
@@ -28,7 +28,7 @@ function incluir(){
     produto.nome = document.getElementById("addnome").value;
     produto.descricao = document.getElementById("adddesc").value;
     produto.valor = document.getElementById("addvalor").value;
-    ajax.open("POST","https://pedro-mota-qua209050.herokuapp.com/api/produto/");
+    ajax.open("POST","http://localhost:8080/api/produto/");
     ajax.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
     ajax.send(JSON.stringify(produto));
     ajax.onload = function(){
@@ -54,7 +54,7 @@ function editarconfirm(){
     pred.nome = document.getElementById("nomeed").value;
     pred.descricao = document.getElementById("desced").value;
     pred.valor = document.getElementById("valored").value;
-    ajax.open("PUT","https://pedro-mota-qua209050.herokuapp.com/api/produto/");
+    ajax.open("PUT","http://localhost:8080/api/produto/");
     ajax.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
     ajax.send(JSON.stringify(pred));
     ajax.onload = function(){
@@ -73,7 +73,7 @@ function apagar(p){
 }
 function apagarconfirm(){
     idApgr=document.getElementById("idapgr").value
-    ajax.open("DELETE","https://pedro-mota-qua209050.herokuapp.com/api/produto/"+idApgr);
+    ajax.open("DELETE","http://localhost:8080/api/produto/"+idApgr);
     ajax.send();
     ajax.onload = function(){
         if(this.status == 200){
